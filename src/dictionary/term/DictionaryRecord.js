d32fce9a50302b319f24bc37d51e268a784321b0
Heorhi Puhachou <h.puhachou@gmail.com>
2022-03-13 11:45:16 +0100

    Styling.

--- src/dictionary/term/DictionaryRecord.js
+++ src/dictionary/term/DictionaryRecord.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './DictionaryRecord.css';
 
 export function DictionaryRecord(dictionary, recordId, text) {
+
+    const getDictionaryName = (dictionaryId) => {
+        if(dictionaryId==='a'){
+            return 'Гласарый перакладчыкаў Ubuntu';
+        }
+        if(dictionaryId==='b'){
+            return 'Proton Terms by Košal&Turok';
+        }
+        if(dictionaryId==='c'){
+            return 'Test dictionary'
+        }
+    }
+
     return (<div className='dictionary-record'
                  key={recordId}>
-        <button className="dictionary-record-info">
+        <div className="dictionary-record-info">
             <div className="dictionary-text-wrapper">
-                {text + '\n© '+dictionary}
+                {text + '\n\n© '+getDictionaryName(dictionary)}
             </div>
-        </button>
+        </div>
     </div>);
 }
